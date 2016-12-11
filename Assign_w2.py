8e264f2b74b87589bd27d51137360e62181ec797
Theresa Macdonald <theresa@MacBook-Air.local>
2016-12-11 16:18:35 +1100

    find and sum numbers in a text document

---
+++ Assign_w2.py
@@ -0,0 +1,21 @@
+import re
+
+sum = 0
+
+hand = open('regex_sum_176084.txt') 
+
+
+for line in hand:
+    line = line.rstrip()
+    answer = re.findall('[0-9]+', line)
+    if not answer:
+        continue
+    else:
+        for ans in answer:
+            sum += int(ans)
+
+print(sum)
+
+
+
+        
